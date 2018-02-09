f85124c4f57ed80aac3cf57ff8500c09f271b820
Fredrik Olsson <fredriko.olsson@gmail.com>
2018-02-09 10:35:04 +0100

    changed path in scripts from home/vagrant to ~

--- provisioning/always.sh
+++ provisioning/always.sh
@@ -4,7 +4,12 @@
 #ssh-add ~/.ssh/GB-OS-key
 #export ANSIBLE_HOST_KEY_CHECKING=False
 
-echo "cd /vagrant" >> /home/vagrant/.bashrc
-echo "eval $(ssh-agent -s)" >> /home/vagrant/.bashrc
-echo "ssh-add ~/.ssh/GB-OS-key" >> /home/vagrant/.bashrc
-echo "export ANSIBLE_HOST_KEY_CHECKING=False" >> /home/vagrant/.bashrc
+# echo "cd /vagrant" >> /home/vagrant/.bashrc
+# echo "eval $(ssh-agent -s)" >> /home/vagrant/.bashrc
+# echo "ssh-add ~/.ssh/GB-OS-key" >> /home/vagrant/.bashrc
+# echo "export ANSIBLE_HOST_KEY_CHECKING=False" >> /home/vagrant/.bashrc
+
+echo "cd /vagrant" >> ~/.bashrc
+echo "eval $(ssh-agent -s)" >> ~/.bashrc
+echo "ssh-add ~/.ssh/GB-OS-key" >> ~/.bashrc
+echo "export ANSIBLE_HOST_KEY_CHECKING=False" >> ~/.bashrc
